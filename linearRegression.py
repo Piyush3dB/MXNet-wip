d3b2978657c38683cb438bcad37fc61c08b0e61e
Piyush Singh <mrsinghpiyush@gmail.com>
2016-05-06 16:05:39 +0100

    Further refactor

--- linearRegression.py
+++ linearRegression.py
@@ -16,11 +16,11 @@ logging.basicConfig(level=logging.DEBUG, format=head)
 ##
 # Generate 101 training points
 ##
-#trX = np.linspace(-1, 1, 101)
-#trY = 2 * trX + np.random.randn(*trX.shape) * 0.33 * 1
+X = np.linspace(-1, 1, 101)
+Y = 2*X + np.random.randn(*X.shape) * 0.33 * 1
 
-X = np.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1])
-Y = np.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+#X = np.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1])
+#Y = np.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3])
 
 if 1:
     trX = X
@@ -58,7 +58,7 @@ batch_end_callback.append(mx.callback.Speedometer(1,1))
 ##
 # Train the model
 ##
-model = mx.model.FeedForward(symbol=net, num_epoch=100, learning_rate=0.01, epoch_size=1, numpy_batch_size=1, ctx=mx.cpu(0))
+model = mx.model.FeedForward(symbol=net, num_epoch=2000, learning_rate=0.01, epoch_size=1, numpy_batch_size=1, ctx=mx.cpu(0))
 print "Start training:"
 model.fit(X=trX, y=trY, batch_end_callback = batch_end_callback)
 
@@ -72,7 +72,7 @@ b = model.arg_params['fullyconnected0_bias'].asnumpy()[0]
 ##
 plt.plot(X, Y, 'ro', label='Original data')
 plt.plot(X, W*X+b, label='Fitted line')
-plt.legend()
+#plt.legend()
 #plt.show()
 
 
