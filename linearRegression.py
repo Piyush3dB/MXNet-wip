80a504680c8498337fe47c3b228991016835bcf7
Piyush Singh <mrsinghpiyush@gmail.com>
2016-05-07 18:57:58 +0100

    typo fix

--- linearRegression.py
+++ linearRegression.py
@@ -5,8 +5,10 @@ import logging
 import pdb as pdb
 import matplotlib.pyplot as plt
 
-USE_ITER = 1
+#np.random.seed(0)
 
+USE_ND_ITER = 1
+USE_CSV_ITER = 0
 ##
 # Setup logging
 ##
@@ -22,12 +24,9 @@ Y = 2*X + np.random.randn(*X.shape) * 0.33 * 1
 #X = np.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,7.042,10.791,5.313,7.997,5.654,9.27,3.1])
 #Y = np.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,1.3])
 
-if USE_ITER:
-    trX = mx.io.NDArrayIter(X, label=Y, batch_size=1, shuffle=True, last_batch_handle='pad')
-    trY = mx.io.NDArrayIter(Y, label=Y, batch_size=1, shuffle=True, last_batch_handle='pad')
-else:
-    trX = X
-    trY = Y
+
+
+
 
 
 
@@ -58,12 +57,28 @@ batch_end_callback.append(mx.callback.Speedometer(1,1))
 ##
 # Train the model
 ##
-model = mx.model.FeedForward(symbol=net, num_epoch=2000, learning_rate=0.01, epoch_size=1, numpy_batch_size=1, ctx=mx.cpu(0))
+model = mx.model.FeedForward(symbol=net, 
+	                         num_epoch=2000, 
+	                         learning_rate=0.01, 
+	                         epoch_size=1,
+	                         numpy_batch_size=1, 
+	                         ctx=mx.cpu(0))
 print "Start training:"
 
-if USE_ITER:
+if USE_ND_ITER:
+    trX = mx.io.NDArrayIter(X, label=Y, batch_size=1, shuffle=True, last_batch_handle='pad')
     model.fit(X=trX,        batch_end_callback = batch_end_callback)
+
+elif USE_CSV_ITER:
+    trX = mx.io.CSVIter(data_csv="./X.csv" , data_shape=(1,),
+                        label_csv="./Y.csv", label_shape=(1,),
+                        batch_size=1)
+    model.fit(X=trX,        batch_end_callback = batch_end_callback)
+
+                           
 else:
+    trX = X
+    trY = Y
     model.fit(X=trX, y=trY, batch_end_callback = batch_end_callback)
 
 W = model.arg_params['fullyconnected0_weight'].asnumpy()[0]
@@ -77,7 +92,7 @@ b = model.arg_params['fullyconnected0_bias'].asnumpy()[0]
 plt.plot(X, Y, 'ro', label='Original data')
 plt.plot(X, W*X+b, label='Fitted line')
 plt.legend()
-plt.show()
+#plt.show()
 
 
 print "Model params after training:"
