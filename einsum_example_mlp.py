9da870c426a9fdacf2fff456e06ac91070930c9e
Piyush Singh <mrsinghpiyush@gmail.com>
2016-05-15 15:19:21 +0100

    Other commits

--- einsum_example_mlp.py
+++ einsum_example_mlp.py
@@ -33,15 +33,21 @@ def func1():
 
 def func2():
     np.random.seed(0)
+
+    # Input tensor: batch size, channels per batch, width per channel, height per channel
+    b = 1
+    c = 1
+    w = 3
+    h = 3
+
+    # Weights tensor: number of feature maps, width, height as above
+    m = 1
     
-    Ni = 3*3
-    Nh = 1*1
-    No = 1*1
+   
+    x = np.random.normal(size=(b*c*w*h)).reshape((b,c,w,h))
     
-    x = np.random.normal(size=(Ni   )).reshape((3,3))
-    
-    W = np.random.normal(size=(Nh,Ni)).reshape((Nh, 3, 3))
-    V = np.random.normal(size=(No,Nh)).reshape((Nh, 1, 1))
+    W = np.random.normal(size=(m*w*h  )).reshape((m, w, h))
+    #V = np.random.normal(size=(No,Nh)).reshape((Nh, 1, 1))
     
     # i -- input dim
     # h -- hidden dim
@@ -49,7 +55,7 @@ def func2():
     
     # Forward prop
     #h = np.einsum('hi,i->h', W, x)
-    h = np.einsum('hjk, jk->h', W, x)
+    h = np.einsum('mwh, bcwh->m', W, x)
     #o = np.einsum('hjk, h->jk', V, h)
     
     print h
