c40033ffb125f426ef55854dcd1628f8e5ba2414
Piyush Singh <mrsinghpiyush@gmail.com>
2016-05-15 15:48:43 +0100

    numpy einsum experiemnts

--- einsum_example_mlp.py
+++ einsum_example_mlp.py
@@ -8,7 +8,7 @@ def func1():
     np.random.seed(0)
     
     Ni = 9
-    Nh = 1*1
+    Nh = 5
     No = 1*1
     # 9,1,1
     #o = [ 6.25436202 ]
@@ -35,18 +35,24 @@ def func2():
     np.random.seed(0)
 
     # Input tensor: batch size, channels per batch, width per channel, height per channel
-    b = 1
+    b = 2
     c = 1
     w = 3
     h = 3
 
     # Weights tensor: number of feature maps, width, height as above
-    m = 1
+    m = 5
     
-   
-    x = np.random.normal(size=(b*c*w*h)).reshape((b,c,w,h))
+    n = np.random.normal(size=(c*w*h)).reshape((c,w,h))
+    x = np.zeros(b*c*w*h).reshape((b,c,w,h))
+    x[0] = n
+    x[1] = n
     
-    W = np.random.normal(size=(m*w*h  )).reshape((m, w, h))
+    n = np.random.normal(size=(m*w*h)).reshape((m,w,h))
+    #pdb.set_trace()
+    W = np.zeros(b*m*w*h).reshape((b,m,w,h))
+    W[0] = n
+    W[1] = n
     #V = np.random.normal(size=(No,Nh)).reshape((Nh, 1, 1))
     
     # i -- input dim
@@ -55,7 +61,7 @@ def func2():
     
     # Forward prop
     #h = np.einsum('hi,i->h', W, x)
-    h = np.einsum('mwh, bcwh->m', W, x)
+    h = np.einsum('bmwh, bcwh->bm', W, x)
     #o = np.einsum('hjk, h->jk', V, h)
     
     print h
